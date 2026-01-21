989487e1fe6302bc4b43649727acc8115c723254
luahana <truepark0@gmail.com>
2026-01-21 15:04:46 -0500

    Revert "refactor(web): use BCP47 for regional locale variants"
    This reverts commit 3a4d84782c39b1b3ce845a65eb865d38a7834a15.

--- web/src/i18n/routing.ts
+++ web/src/i18n/routing.ts
@@ -2,28 +2,27 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   // 20 supported languages
-  // Using short codes for most languages, BCP47 for regional variants
   locales: [
-    'en',     // English (all regions)
-    'zh-CN',  // Chinese (Simplified)
-    'es',     // Spanish
-    'ja',     // Japanese
-    'de',     // German
-    'fr',     // French
-    'pt-BR',  // Portuguese (Brazilian)
-    'ko',     // Korean
-    'it',     // Italian
-    'ar',     // Arabic
-    'ru',     // Russian
-    'id',     // Indonesian
-    'vi',     // Vietnamese
-    'hi',     // Hindi
-    'th',     // Thai
-    'pl',     // Polish
-    'tr',     // Turkish
-    'nl',     // Dutch
-    'sv',     // Swedish
-    'fa',     // Persian (Farsi)
+    'en',  // English
+    'zh',  // Chinese (Simplified)
+    'es',  // Spanish
+    'ja',  // Japanese
+    'de',  // German
+    'fr',  // French
+    'pt',  // Portuguese
+    'ko',  // Korean
+    'it',  // Italian
+    'ar',  // Arabic
+    'ru',  // Russian
+    'id',  // Indonesian
+    'vi',  // Vietnamese
+    'hi',  // Hindi
+    'th',  // Thai
+    'pl',  // Polish
+    'tr',  // Turkish
+    'nl',  // Dutch
+    'sv',  // Swedish
+    'fa',  // Persian (Farsi)
   ],
   defaultLocale: 'en',
   localePrefix: 'always'
